056d53b93b443f90bce1f9e476fe44634511b94b
Jin Young Kim <wlsdud66523@kakao.com>
2026-07-16 07:27:42 +0900

    コネクションプールとデータソース理解- データソース 例2 - コネクションプール

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,6 +1,7 @@
 package hello.jdbc.connection;
 
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -37,9 +38,30 @@ public class ConnectionTest {
 
     // 사용 영역
     private void updateDataSource(DataSource dataSource) throws SQLException {
-        Connection connection1 = dataSource.getConnection();
+        Connection connection1 = dataSource.getConnection(); // 최초로 커넥션 획득 요청이 오면 커넥션 풀 커넥션이 생성된다.
         Connection connection2 = dataSource.getConnection();
         log.info("conncetion={}, class={}",connection1,connection1.getClass());
         log.info("conncetion={}, class={}",connection2,connection2.getClass());
     }
+
+    @Test
+    // 이번에는 커넥션 풀을 통해 커넥션을 획득
+    void dateSourceConnectionPool() throws SQLException, InterruptedException {
+        // 설정 영역
+        HikariDataSource dataSource = new HikariDataSource(); // 스프링 부트가 Hikari를 기본 등록
+        dataSource.setJdbcUrl(ConnectionConst.URL);
+        dataSource.setUsername(ConnectionConst.USERNAME);
+        dataSource.setPassword(ConnectionConst.PASSSWORD);
+
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        // 사용 영역
+        updateDataSource(dataSource); // 이 과정에서 Connectino pool에 커넥션들이 생성된다.
+        Thread.sleep(1000); // 커넥션 풀 생성 시에는 애플리케이션의 뜨는 속도에 영향을 미치지 않게 하기 위해 별도의 쓰레드로 실행을 한다.
+    }                            //  그래서 updateDataSource 메서드를 실행시키는 쓰레드가 실행 종료될 까지 기다리게 하기 위하여 이 코드를 넣었다.
+                                 // 그래야 커넥션이 생성되고 있는 로그를 볼 수가 있끼 떄문이다.
+                                 // 왜냐하면 애플리케이션 쓰레드가 종료되버리면 프로그램이 종료되니깐 로그가 도중에 짤려 버린다
+
+
 }
